65ff0755ffb839351f2fbf72b0451c5b8a52d538
Andre Oliveira <andreoliveira@MacBook-Pro-de-Andre-2.local>
2014-04-25 19:10:39 -0300

    commit foda do andre

--- citymovies/app/models/movie.rb
+++ citymovies/app/models/movie.rb
@@ -5,5 +5,5 @@ class Movie < ActiveRecord::Base
   has_many :cinema_movies
   has_many :cinemas, through: :cinema_movies
   has_attached_file :cover, :styles => { large: '500x500', medium: '300x300>', thumb: '100x100>' }, default_url: '/images/no-found.gif'
-  validates_attachment :cover, :presence => true,:content_type => { content_type: /\Aimage\/.*\Z/ }, size: { in: 0..500.kilobytes }
+  validates_attachment :cover, :presence => true,:content_type => { content_type: /\Aimage\/.*\Z/ }, size: { in: 0..2000.kilobytes }
 end
